c84fb65879e80200bc6c1b5b35f95cdfcd8f7c4f
chrisHub68 <chris.41@hotmail.de>
2015-11-17 10:49:49 +0100

    validation.js player_entry.html, created javascript file for validation, Meilenstein3 c.hansen
    java script Datei hinzugefügt und player_entry.html eingebunden

---
+++ Meilenstein3/validation.js
@@ -0,0 +1,40 @@
+
+	function validate(){
+		
+	var mistake = false;
+	var name = document.getElementById("name");
+	var vorname = document.getElementById("vorname");
+	var verein = document.getElementById("verein");
+	var hcoach = document.getElementById("hcoach");
+	var acoach = document.getElementById("acoach");
+	var number = document.getElementById("number");
+	var jahr = document.getElementById("jahr");
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+	if(mistake == true){
+	
+	window.alert("Einige Eingaben sind fehlerhaft.\nBitte überprüfen Sie ihre Eingaben");
+	return false;
+
+	}else{
+		
+		return true;
+	}
+	
+	
+	
+	
+	
+}
