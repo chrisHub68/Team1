b77cff3a66e1873c6ceb46f6a84ca5453fbf333d
hmAhmetovic <hamidahmetovic@web.de>
2015-11-22 12:25:43 +0100

    player_detail.html stylesheetFavoriten.css loadTable.js, implemented logic for loading table, Meilenstein 4, hmAhmetovic

--- Meilenstein4/loadTable.js
+++ Meilenstein4/loadTable.js
@@ -1,4 +1,6 @@
 
+var arrayPlayers = [];
+var arrayFavorites = [];
 
 function loadAllPlayers(){
 	
@@ -6,10 +8,11 @@ function loadAllPlayers(){
 	document.getElementById("meineFavoriten").style.backgroundColor = "rgb(153,217,234)";
 	document.getElementById("meineFavoriten").style.color="#000000";
 	
-	//Setzt bei Click auf "AlleSpieler" Textfarbe:weiß und BGColor:schwarz
+	//Setzt "AlleSpieler" Textfarbe:weiß und BGColor:schwarz
 	document.getElementById("alleSpieler").style.color="#FFFFFF";
 	document.getElementById("alleSpieler").style.backgroundColor="rgb(0,38,255)";
-	
+
+	loadTable(arrayPlayers);
 	
 }
 
@@ -21,4 +24,76 @@ function loadFavorites(){
 	document.getElementById("meineFavoriten").style.backgroundColor = "rgb(0,38,255)"; 	
 	document.getElementById("meineFavoriten").style.color= "#FFFFFF";
 	
+	loadArrayFavorites();
+	loadTable(arrayFavorites);
+	
+}
+
+// Lädt alle Spieler mit Favorite:true in ein Array
+function loadArrayFavorites(){
+
+	var count = 0;
+	
+	for(var player=0; player < arrayPlayers.length; player++){
+		if(arrayPlayers[player].isFavorite == true){
+			arrayFavorites[count++] = arrayPlayers[player];
+		}
+	}	
+}
+
+function loadTable(loadthis){
+	
+	clearTable();
+	
+	var table = document.getElementById("spieler");	
+	var row;
+
+	for(var i=0; i< loadthis.length; i++){
+
+		row = table.insertRow(-1);
+		
+		cell1 = row.insertCell(0);
+		cell2 = row.insertCell(1);
+		cell3 = row.insertCell(2);
+		cell4 = row.insertCell(3);
+		cell5 = row.insertCell(4);
+		cell6 = row.insertCell(5);
+		cell7 = row.insertCell(6);
+		cell8 = row.insertCell(7);
+
+		cell1.innerHTML = loadthis[i].firstname + loadthis[i].surname	;
+		cell2.innerHTML = loadthis[i].team;
+		cell3.innerHTML = loadthis[i].headcoach;
+		cell4.innerHTML = loadthis[i].asisstantcoach;
+		cell5.innerHTML = loadthis[i].position;
+		cell6.innerHTML = loadthis[i].isActive;
+		cell7.innerHTML = loadthis[i].number;
+		cell8.innerHTML = loadthis[i].year;
+	}
+}
+
+//löscht alle Zeilen aus einer Tabelle
+function clearTable(){
+	
+	var parent = document.getElementById("spieler");
+    while(parent.hasChildNodes())
+    {
+       parent.removeChild(parent.firstChild);
+    }
+		
+}
+
+function loadJSON(){
+	
+	var xml = new XMLHttpRequest();	
+	
+	xml.onreadystatechange = function(){
+		if (xml.readyState == 4 && xml.status == 200) {
+			arrayPlayers = JSON.parse(xml.responseText);
+			loadAllPlayers();
+		}
+	};
+	
+	xml.open("GET", "data.json" , true);
+    xml.send();		
 }
